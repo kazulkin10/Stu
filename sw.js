c3389e219956b21fdf937e39c56595132b0500c4
Egor <kazulkin10@gmail.com>
2025-09-20 16:28:18 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,3 @@
+const CACHE='naste-v2';
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['/','/index.html','/styles.css','/app.js'])))});
+self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
